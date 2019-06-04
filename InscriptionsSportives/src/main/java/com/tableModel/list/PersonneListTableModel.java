66ef48df991237ad0d0e7e77956416e5f3edd9d0
Tao BERQUER <tao.berquer@gmail.com>
2019-06-04 02:38:40 +0200

    Ajout d'une colonne dans le tableau PErsonne.

--- InscriptionsSportives/src/main/java/com/tableModel/list/PersonneListTableModel.java
+++ InscriptionsSportives/src/main/java/com/tableModel/list/PersonneListTableModel.java
@@ -9,7 +9,7 @@ public class PersonneListTableModel extends ListTableModel {
 	private static final long serialVersionUID = -8622839069400977036L;
 	
 	protected List<Personne> list;
-	protected String[] columnName = {"Nom", "Email"};
+	protected String[] columnName = {"Nom", "Prenom", "Email"};
 	
 	protected PersonneListTableModel() {}
 	
@@ -40,7 +40,8 @@ public class PersonneListTableModel extends ListTableModel {
 	public Object getValueAt(int rowIndex, int index) {
         switch (index) {
 	        case 0 : return list.get(rowIndex).getNom();
-	        case 1 : return list.get(rowIndex).getMail();
+	        case 1 : return list.get(rowIndex).getPrenom();
+	        case 2 : return list.get(rowIndex).getMail();
 	    }
 	    return null;
 	}
